4c4838a9587b23263c92ed18deac3a1ed2cf6a75
Anandhan <anandmars98@gmail.com>
2024-12-13 15:44:43 +0530

    initial commit changes

--- src/pages/productsForm/DimensionForm.jsx
+++ src/pages/productsForm/DimensionForm.jsx
@@ -31,10 +31,9 @@ const DimensionForm = ({ onSubmit }) => {
 
     }, []);
 
-    
 
     // if (dimensions.userPhoto) {
-    //     console.log("Old photo URL revoked:", URL.createObjectURL(dimensions.userPhoto));
+    //     console.log("Old photo URL revoked is:", URL.createObjectURL(dimensions.userPhoto));
     // }
 
 
